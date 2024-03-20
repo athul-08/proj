aad50c0b45451829f8edab7df1ff24b440dc85ea
athul-08 <athulprasad0812@gmail.com>
2024-03-20 12:33:09 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomeComponent,
+        path: ''
+    },
+    {
+        component: ContactComponent,
+        path: 'contact'
+    },
+    {
+        component: ProductComponent,
+        path: 'product'
+    },
+    {
+        component: AboutComponent,
+        path: 'about'
+    },
+    {
+        component: HomeComponent,
+        path: '**'
+    }
+];
